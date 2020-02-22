9c91f583129b373918a30ab7ad16de98472a7eb6
Ravi Kant Pujari <rvkantpujari@gmail.com>
2020-02-22 14:47:04 +0530

    Feb 22, 2020

---
+++ sed/sed_sh.sh
@@ -0,0 +1,4 @@
+# sed 's/find/replace/' filename.ext
+# sed -e 's/find/replace/; s/find/replace/; ...' filename.ext
+# Words to be replace are fetched from file (file.ext)
+# sed -f file.txt filename.ext
